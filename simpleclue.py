ac347fa61aa33c55336f5cbc690520b9c6f5a904
Mark Nesbitt <mwnesbitt@gmail.com>
2016-11-06 21:45:55 -0500

    New update

--- simpleclue.py
+++ simpleclue.py
@@ -1,21 +1,31 @@
 import sys
 import simplecluestrat
 
-def runGame(list):
+def runGame(gameStyle, list):
   #masterhistory is the absolute state of the game, but is selectively revealed to players on their turn.  
   #A strategy can choose to make use of as much or as little of that info as it wants to.
-  masterhistory = [list[0],[list[1],list[2],list[3]],[]]
+  masterhistory = [list[0],[list[1],list[2],list[3]],[]] #assuming only 3 players for now.  
   n = 0
   gameOver = False
   while not gameOver:
-    cleanedhistory = simplecluestrat.cleanMasterHistory(n,masterhistory) #for player n, hide all the things that player n would not have seen
-    guess = simplecluestrat.dontBeDumb(cleanedhistory) #player n makes a guess based on his/her strategy, currently hardcoded to dontBeDumb.  Should make this a param.
-    guessresult = simplecluestrat.evaluateGuess(n, guess) #the player and the card that respond to the guess
-    masterhistory[2].append([n,guess,guessresult]) #update master history with that information
+    tempjunk = input("Enter the player number:")
+    if(tempjunk is "x"):
+      break
+    if(int(tempjunk)>n):
+      n=int(tempjunk)
+    cleanedhistory = simplecluestrat.cleanMasterHistory(n%3,masterhistory) #for player n, hide all the things that player n would not have seen
+    if(gameStyle ==1):
+      guess = simplecluestrat.inputGuess()
+      guessresponse = simplecluestrat.inputGuessResponse()
+    else:
+      guess = simplecluestrat.dontBeDumb(cleanedhistory) #player n makes a guess based on his/her strategy, currently hardcoded to dontBeDumb.  Should make this a param.
+      guessresponse = simplecluestrat.evaluateGuess(n, guess, masterhistory) #the player and the card that respond to the guess
+    masterhistory[2].append([n,guess,guessresponse]) #update master history with that information
     n=n+1
-    #check to see if the last guess was correct.  If it was, then change gameOver to true. 
-    if(n==3): 
-      gameOver = True
+    print(guessresponse)
+    print(guessresponse == ['x',['x','x']])
+    if(guessresponse == ['x',['x','x']]):
+      gameOver=True
   for item in masterhistory:
     print(item)
 
@@ -31,14 +41,20 @@ def main():
     sys.exit(0)  
   #outcome = runGame(sys.argv)
   #Shuffle and deal the cards now.  Use runGame for the mechanics. 
-  answer = [[0,0],[1,0],[2,0]]
   #assuming only 3 players for now to keep things simple
   player0 = [[0,1],[1,1],[2,1]]
-  player1 = [[0,1],[1,1],[2,1]]
-  player2 = [[0,1],[1,1],[2,1]]
-  runGame([answer, player0, player1, player2])
+  player1 = [[0,1],[1,1],[2,2]]
+  player2 = [[0,1],[1,1],[2,2]]
+  gameStyle = int(input("1 for manual, 2 for computer-run:")) #Note: not planning on building out option #2.  The bones are all there to do it, though.
+  if(gameStyle==1):
+    x="x"
+    answer=[[x,x],[x,x],[x,x]]
+  else:
+    answer = [[0,0],[1,0],[2,0]]
+  #The player to the left of the person using the program is player 0.
+  runGame(gameStyle, [answer, player0, player1, player2])
 
-#NOTE: need a quick and easy way to build up the game history so that you can actually use this to play clue. 
-#NOTE: you may not end up finishing runGame because you really only want to use this to play a game of Clue. 
+#NOTE: would love to have this data structure reviewed by an experienced code.  There has GOT to be a better way of doing this.
+#NOTE: need to have a way where if you mistype, it doesn't wreck the code.
 if __name__ == '__main__':
   main()
--- simplecluestrat.py
+++ simplecluestrat.py
@@ -22,17 +22,67 @@ guess format:
 """
 
 def dontBeDumb(cleanedhistory):
-  #this needs to take in only the facts, and then it makes a best guess given those facts
+  #First step should be to compile a list of all possible guesses
+  #Then remove all guesses that contain a card that you've seen
+  #The above is one option, another would be to try to make a map of where all the cards are.  Then you can try to build guesses based off that map
   
   return [[0,0],[1,0],[2,1]]
 
 def cleanMasterHistory(playernumber, masterhistory):
   #needs to mask everything that playernumber should not have been able to see, censor it for that player
-  return masterhistory
+  cleanedhistory=masterhistory[:]
+  x="x"
+  cleanedhistory[0]=[[x,x],[x,x],[x,x]]
+  allplayerscards=masterhistory[1]  
+  cleanedhistory[1]=allplayerscards[playernumber]  #this needs to be changed to put x's in for other players cards to maintain data structure
+  turnhistory=masterhistory[2]
+  for turn in turnhistory:
+    if(turn[0] != playernumber):
+      response = turn[2]
+      response[1]=[x,x]
+      turn[2]=response
+  cleanedhistory[2]=turnhistory
+  return cleanedhistory
   
-def evaluateGuess(playernumber, guess):
+def evaluateGuess(playernumber, guess, masterhistory):
   #This takes the player number and the guess, and returns the player number disproving it and the disproving card
-  #for now if the disproving player can disprove with 2 cards, the game will randomly pick which card (actually probably pick the first card).
+  #for now if the disproving player can disprove with 2 cards, the game will pick the first card to show.
   #In the future, however, you could have a strategy here that you call to decide which card to show.
-  #Don't forget to code what happens if the guess is correct.  Maybe just pass back x's or something. 
-  return [1, [0, 0]]
+  allplayerscards = masterhistory[1]
+  respondingplayer = playernumber + 1
+  while(respondingplayer != playernumber): 
+    relevantcards=allplayerscards[respondingplayer]
+    if(guess[0] in relevantcards):
+      return [respondingplayer, guess[0]]
+    if(guess[1] in relevantcards):
+      return [respondingplayer, guess[1]]
+    if(guess[2] in relevantcards):
+      return [respondingplayer, guess[2]]
+    respondingplayer = (respondingplayer+1)%3 #need a parameter for number of players, assuming 3 for now while building. 
+  return ["x",["x","x"]] #no one can disprove the guess, i.e. the guess was correct.
+      
+  #return [1, [0, 0]]
+
+def inputGuess():
+  character = int(input("What character number?"))
+  weapon = int(input("What weapon number?"))
+  room = int(input("What room number?"))
+  return [[0,character],[1,weapon], [2, room]]
+  
+def inputGuessResponse():
+  tempjunk= input("Enter the player number that responded to the guess:")
+  if(tempjunk is ""):
+    playernumber = "x"
+  else:
+    playernumber = int(tempjunk)
+  tempjunk = input("Enter 0 for a character, 1 for weapon, 2 for room (leave blank if you don't know):")
+  if(tempjunk):
+    card0 = int(tempjunk)
+  else:
+    card0 = "x"
+  tempjunk = input("Enter 0 through n for which character/weapon/room it was (leave blank if you don't know):")
+  if(tempjunk):
+    card1 = int(tempjunk)
+  else:
+    card1 = "x"
+  return [playernumber, [card0,card1]]
